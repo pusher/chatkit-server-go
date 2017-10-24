64f81f14a974d3dc763263a536f105816646fe81
charlie cochrane <cochrane26@gmail.com>
2017-10-24 17:08:18 +0100

    added some more token tests

--- token.go
+++ token.go
@@ -7,6 +7,52 @@ import (
 	jwt "github.com/dgrijalva/jwt-go"
 )
 
+// NewChatkitSUToken produces a new jwt su token that will be compatible with Chatkit
+func NewChatkitSUToken(appID string, keyID string, keySecret string, expiryDuration time.Duration) (tokenString string, expiry time.Time, err error) {
+	jwtClaims, tokenExpiry := getGenericTokenClaims(appID, keyID, expiryDuration)
+
+	jwtClaims["su"] = true
+
+	tokenString, err = signToken(keySecret, jwtClaims)
+	return tokenString, tokenExpiry, err
+}
+
+// NewChatkitUserToken produces a new jwt user token that will be compatible with Chatkit
+func NewChatkitUserToken(appID string, keyID string, keySecret string, userID string, expiryDuration time.Duration) (tokenString string, expiry time.Time, err error) {
+	jwtClaims, tokenExpiry := getGenericTokenClaims(appID, keyID, expiryDuration)
+
+	jwtClaims["sub"] = userID
+
+	tokenString, err = signToken(keySecret, jwtClaims)
+	return tokenString, tokenExpiry, err
+}
+
+func getGenericTokenClaims(appID string, keyID string, expiryDuration time.Duration) (jwtClaims jwt.MapClaims, tokenExpiry time.Time) {
+	timeNow := time.Now()
+	tokenExpiry = timeNow.Add(expiryDuration)
+
+	jwtClaims = jwt.MapClaims{
+		"app": appID,
+		"iss": "api_keys/" + keyID,
+		"iat": timeNow.Unix(),
+		"exp": tokenExpiry.Unix(),
+	}
+
+	return jwtClaims, tokenExpiry
+}
+
+func signToken(keySecret string, jwtClaims jwt.MapClaims) (tokenString string, err error) {
+	// Create a new access token object, specifying signing method and the claims
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwtClaims)
+
+	// Sign using the keySecret and get the complete encoded token as a string
+	tokenString, err = token.SignedString([]byte(keySecret))
+	if err != nil {
+		return "", err
+	}
+	return tokenString, nil
+}
+
 type tokenManager interface {
 	getToken() (string, error)
 }
@@ -45,48 +91,4 @@ func (stm *suTokenManager) getToken() (string, error) {
 		stm.token = tokenString
 	}
 	return stm.token, nil
-}
-
-func NewChatkitSUToken(appID string, keyID string, keySecret string, expiryDuration time.Duration) (tokenString string, expiry time.Time, err error) {
-	jwtClaims, tokenExpiry := getGenericTokenClaims(appID, keyID, expiryDuration)
-
-	jwtClaims["su"] = true
-
-	tokenString, err = signToken(keySecret, jwtClaims)
-	return tokenString, tokenExpiry, err
-}
-
-func NewChatkitUserToken(appID string, keyID string, keySecret string, userID string, expiryDuration time.Duration) (tokenString string, expiry time.Time, err error) {
-	jwtClaims, tokenExpiry := getGenericTokenClaims(appID, keyID, expiryDuration)
-
-	jwtClaims["sub"] = userID
-
-	tokenString, err = signToken(keySecret, jwtClaims)
-	return tokenString, tokenExpiry, err
-}
-
-func getGenericTokenClaims(appID string, keyID string, expiryDuration time.Duration) (jwtClaims jwt.MapClaims, tokenExpiry time.Time) {
-	timeNow := time.Now()
-	tokenExpiry = timeNow.Add(expiryDuration)
-
-	jwtClaims = jwt.MapClaims{
-		"app": appID,
-		"iss": "api_keys/" + keyID,
-		"iat": timeNow.Unix(),
-		"exp": tokenExpiry.Unix(),
-	}
-
-	return jwtClaims, tokenExpiry
-}
-
-func signToken(keySecret string, jwtClaims jwt.MapClaims) (tokenString string, err error) {
-	// Create a new access token object, specifying signing method and the claims
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwtClaims)
-
-	// Sign using the keySecret and get the complete encoded token as a string
-	tokenString, err = token.SignedString([]byte(keySecret))
-	if err != nil {
-		return "", err
-	}
-	return tokenString, nil
 }
--- token_test.go
+++ token_test.go
@@ -3,10 +3,51 @@ package chatkitServerGo
 import (
 	"fmt"
 	"testing"
+	"time"
 
 	jwt "github.com/dgrijalva/jwt-go"
 	"github.com/stretchr/testify/assert"
 )
+
+func TestNewChatkitSUToken(t *testing.T) {
+	token, expiry, err := NewChatkitSUToken("appID", "keyID", "keySecret", time.Hour)
+	assert.NoError(t, err, "expect no error")
+
+	assert.False(t, time.Now().After(expiry), "expiry should be after now")
+
+	parsedToken, err := jwt.Parse(token, func(token *jwt.Token) (interface{}, error) {
+		// Validate the signing alg is HMAC-SHA256:
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+
+		// return the key to be parsed with
+		return []byte("keySecret"), nil
+	})
+
+	assert.NoError(t, err, "expect no error when parsing the token")
+	assert.True(t, parsedToken.Valid, "token produced was invalid")
+}
+
+func TestNewChatkitUserToken(t *testing.T) {
+	token, expiry, err := NewChatkitUserToken("appID", "keyID", "keySecret", "bob", time.Hour)
+	assert.NoError(t, err, "expect no error")
+
+	assert.False(t, time.Now().After(expiry), "expiry should be after now")
+
+	parsedToken, err := jwt.Parse(token, func(token *jwt.Token) (interface{}, error) {
+		// Validate the signing alg is HMAC-SHA256:
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+
+		// return the key to be parsed with
+		return []byte("keySecret"), nil
+	})
+
+	assert.NoError(t, err, "expect no error when parsing the token")
+	assert.True(t, parsedToken.Valid, "token produced was invalid")
+}
 
 func TestTokenManagerGetTokenNew(t *testing.T) {
 	tokenManager := newTokenManager("testApp", "keyID", "keySecret")
