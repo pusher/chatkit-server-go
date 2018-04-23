3fd89eef295b866690f65799124c2eae46802889
Hamilton Chapman <hamchapman@gmail.com>
2018-04-23 18:14:17 +0100

    Add auth_example as an example of using the new Authenticate function

---
+++ auth_example/authenticate.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/pusher/chatkit-server-go"
+)
+
+func main() {
+	instanceLocator := os.Getenv("CHATKIT_INSTANCE_ID")
+	key := os.Getenv("CHATKIT_KEY")
+	if instanceLocator == "" || key == "" {
+		log.Fatalln("Please set the CHATKIT_INSTANCE_ID and CHATKIT_KEY environment variables to run the example")
+	}
+
+	serverClient, err := chatkit.NewClient(instanceLocator, key)
+	handleErr("Instantiating Client", err)
+
+	log.Println("Authenticating")
+	authRes := serverClient.Authenticate("ham")
+	log.Println(authRes.Status, authRes.Headers, authRes.Body)
+}
+
+func handleErr(descrip string, err error) {
+	if err != nil {
+		log.Fatalln("Error ", descrip, err)
+	}
+}
